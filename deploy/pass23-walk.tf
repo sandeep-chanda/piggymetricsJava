e3eab1cb3f20f9b2928bd692d6da9759b5a670f2
sandeep-chanda <sandeepchanda@gmail.com>
2026-09-23 13:34:17 +0530

    Pass 23 walk: remove the terraform file the tree walker does not list.

--- deploy/pass23-walk.tf
+++
@@ -1,5 +0,0 @@
-# Pass 23 walk. Names one component the SignatureFinance read already holds.
-resource "azurerm_kubernetes_cluster" "pass23_walk" {
-  name                = "aks-harnessmatters-eus2"
-  resource_group_name = "rg-harnessmatters"
-}
